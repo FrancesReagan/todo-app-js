6506f5183a9c459d0dc2363814a17444c37a34b9
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 15:26:39 -0400

    added comments to filtercontext

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -4,16 +4,21 @@ import { createContext, useContext, useState } from "react"
 const FilterContext = createContext(null);
 
 export function FilterProvider({children}) {
+  // state to track current filter (all, active, completed)
   const [filter, setFilter] = useState("all");
+  // value object to share with components//
   const value = {filter, setFilter};
-  return <FilterContext.Provider value={value}>{children}</FilterContext.Provider>;
-
+  return (
+  <FilterContext.Provider value={value}>
+    {children}
+    </FilterContext.Provider>
+  );
 }
 
 export function useFilterContext(){
   const context =useContext(FilterContext);
   if (!context){
-    throw new Error("useFilterContext must be used withina FilterProvider");
+    throw new Error("useFilterContext must be used within a FilterProvider");
   }
   return context;
 
