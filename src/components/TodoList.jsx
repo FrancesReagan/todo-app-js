228a499db034ccf51495d9c4bb6dc9a25bbe6489
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 13:54:27 -0400

    fixed todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,48 @@
-import { useTodos } from "../context/TodoContext"
-import TodoItem from "./TodoItem"
+import { useTodoContext } from "../contexts/TodoContext";
+import { useFitlerContext } from "../contexts/FilterContext";
+import TodoItem from "./TodoItem";
 
+export default function TodoList(){
+  const { todos } = useTodoContext();
+  const { filter } = useFilterContext();
 
+  const filteredTodos = todos.filter((todo) => {
+     switch (filter) {
+      case "active":
+        return !todo.completed;
+        case "completed":
+          return todo.completed;
+        default:
+          return true;
+  }
+    });
 
-function TodoList() {
+    return (
+      <div>
+        {filteredTodos.length > 0 ? (
+          filteredTodos.map((todo) => <TodoItem key={todo.id}={todo}/>)
+        ) : (
 
-  const {todos} = useTodos() 
+          <p>No todos yet! Add one above.</p>
+        )}      
+      </div>
+    );
+  }
 
-  return (
-<>
-  <ul>
-    {todos && todos.map(todo => (
-      <TodoItem key = {todo.id} 
-    todo={todo}/>
-  ))} </>
-  </ul>
+// function TodoList() {
+
+//   const {todos} = useTodos() 
+
+//   return (
+// <>
+//   <ul>
+//     {todos && todos.map(todo => (
+//       <TodoItem key = {todo.id} 
+//     todo={todo}/>
+//   ))} </>
+//   </ul>
   
 
-  );
-}
-export default TodoList;
+//   );
+// }
+// export default TodoList;
