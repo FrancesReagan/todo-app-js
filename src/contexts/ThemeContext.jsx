79e0656b0688e49066f868f47dd73b2f6b494a10
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 12:13:41 -0400

    added error handling to ThemeContext

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,1 +1,20 @@
 // theme context switches the light/dark mode//
+import { createContext, useContext, useState, useEffect }  from "react";
+
+const ThemeContext = createContext(null);
+export function ThemeProvider({children}) {
+  const [theme, setTheme] = useState(() =>{
+    try {
+      return localStorage.getItem("theme") || "light";
+
+    } catch (error) {
+      console.error("Failed to get theme:", error);
+      return "light";
+    }
+  });
+
+  
+
+
+
+}
