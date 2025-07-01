7296adf7ff20ddbc05a7ec43952aa3d2b5f48d82
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 19:15:22 -0400

    added more comments to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// main--is the starting point that renders the app with providers//
+// main--is the starting point that renders the app and wraps it with the providers//
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // global styles//
@@ -14,3 +14,7 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>
   
 );
+
+// more notes on Main.jsx---finds the root element in the HTML file (like a blank canvas)---
+// renders the App inside AppProviders, which gives it access to all contexts.----
+// StrictMode--is a react tool that helps catch mistakes in my code.--//
