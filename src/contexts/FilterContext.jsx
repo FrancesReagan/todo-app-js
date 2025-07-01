ddbbc2fe6ab0bdc95d6f25761b84a2d3cf2b6855
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 17:53:05 -0400

    added more comments on filtercontext

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -1,4 +1,8 @@
 // filter context controls which tasks show (all, active, completed)//
+// filter context is the control center for which tasks will show as stated above---it stores the current filter
+// choice(all or active or completed) in state---it provides a function to change the filter---and other components
+// like the task list use this to choose what to display--//
+
 import { createContext, useContext, useState } from "react"
 
 const FilterContext = createContext(null);
@@ -23,3 +27,8 @@ export function useFilterContext(){
   return context;
 
 }
+
+// more notes on filtercontext----state: const[filter,setFilter] is a box that holds the current filter (all) and a 
+// tool (setFilter) to change it.  context--filtercontext--is another shared notebook--yet this one is focused on 
+// which tasks to show...provider---shares the filter and setFilter function with other components----filter context
+// is simple--as it manages only one piece of info.//
