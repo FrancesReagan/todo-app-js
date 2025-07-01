45ec67f69914421a836bbd58c989f9816b8aba21
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 14:36:02 -0400

    fixed filterbutton component

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,7 +1,8 @@
-// put export default function at beginning//
+// put export default function at beginning---as this function returns JSX---its a component not a hook--
+// so not using the hook naming convention of useFilterButtons//
 import { useFilterContext } from "../contexts/FilterContext";
 
-export default function useFilterButtons(){
+export default function FilterButtons(){
   const {filter, setFilter} = useFilterContext();
 
   return (
@@ -11,6 +12,11 @@ export default function useFilterButtons(){
         >
           All
         </button>
+        <button onClick={() => setFilter("active")}
+        style={{fontWeight: filter === "completed"?"bold": "normal"}}
+        >
+          Completed
+        </button>
     </div>
-  )
+  );
 }
